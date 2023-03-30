2f9ff718d394cd179cb298a7d9f33eac4a9360cf
Ecem <116415588+tugbaecemyildiz@users.noreply.github.com>
2023-03-31 01:07:27 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ogrenciBilgiSistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kadi, sifre;
+            kadi = textBox1.Text;
+            sifre = textBox2.Text;
+            if (kadi == "admin" && sifre == "123") 
+            {
+                MessageBox.Show("Hoşgeldiniz", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ogrenciBilgileri frm = new ogrenciBilgileri();
+                frm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı Girişi Hatalı", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox1.Clear();
+                textBox2.Clear();
+            }
+        }  
+    }
+}
---
+++ ogrenciBilgileri.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ogrenciBilgiSistemi
+{
+    public partial class ogrenciBilgileri : Form
+    {
+        public ogrenciBilgileri()
+        {
+            InitializeComponent();
+        }
+        double not1, not2, not3, ort;
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+            {
+                not1 = Convert.ToDouble(textBox1.Text);
+                not2 = Convert.ToDouble(textBox2.Text);
+                not3 = Convert.ToDouble(textBox3.Text);
+                ort = (not1 + not2 + not3) / 3;
+                label4.Text = ort.ToString();
+
+                if (ort >= 50)
+                {
+                    label5.BackColor = Color.Green;
+                    label5.Text = "GEÇTİ";
+                }
+                else
+                {
+                    label5.BackColor = Color.Red;
+                    label5.Text = "KALDI";
+                }
+            }  
+            else
+            {
+                MessageBox.Show("Boş Bırakılamaz", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
